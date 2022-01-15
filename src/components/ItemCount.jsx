78aa1838dff9b8189fd487b059f53604539d837a
matiasfmart <matiasfmart@MacBook-Pro-de-Matias.local>
2022-01-15 15:45:57 -0300

    details

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -4,8 +4,8 @@ function ItemCount({stock, initial, onAdd}) {
 
     const [count, setCount] = useState(initial ? initial : 0);
 
-    function add(){ count ? setCount(count - 1) : setCount(0) }
-    function remove(){ count < stock ? setCount(count + 1) : setCount(count) }
+    function remove(){ count ? setCount(count - 1) : setCount(0) }
+    function add(){ count < stock ? setCount(count + 1) : setCount(count) }
     function toOnAdd(){
         count ? onAdd(count) : alert('No ingreso productos a su compra');
     }
@@ -18,9 +18,9 @@ function ItemCount({stock, initial, onAdd}) {
                     <h2>Ejemplo</h2>
                 </div>
                 <div className="container-btnCart">
-                    <button onClick={()=> add() } className="btn-remove">-</button>
+                    <button onClick={()=> remove() } className="btn-remove">-</button>
                     <h2 className="count">{count}</h2>
-                    <button onClick={()=> remove() } className="btn-add">+</button>
+                    <button onClick={()=> add() } className="btn-add">+</button>
                 </div>
                 <div className="container-btnAddToCart">
                     <button onClick={()=> toOnAdd()} className="btn-addToCart">Agregar</button>
