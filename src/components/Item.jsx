8393ff607f2143cefc858368d17be94db3b8b591
matiasfmart <matiasfmart@MacBook-Pro-de-Matias.local>
2022-01-25 00:41:10 -0300

    add detail button

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,5 @@
 import ItemCount from "./ItemCount";
+import { Link } from "react-router-dom";
 
 function Item({products}){
     
@@ -14,6 +15,7 @@ function Item({products}){
                 <p>${products.price}</p>
             </div>
             <ItemCount stock={10} initial={1} onAdd={onAdd}/>
+            <Link to={`/Item/${products.id}`} >Detalle</Link>
         </>
     )
 }
