78be6368091f6c3d611be5847dd67ec356ee5747
matiasfmart <matiasfmart@MacBook-Pro-de-Matias.local>
2022-02-04 18:02:24 -0300

    desafio context terminado

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { cartContext } from '../context/CartProvider';
 
 function Cart(){
+
+  const { cart } =  useContext(cartContext);
+
   return (
     <>
-        
+        {
+          cart.map((e) => (
+            <div className='container-itemCart'>
+              <div className='container-itemCart__img'>
+                <img src={e.item.urlPicture} alt="" />
+              </div>
+              <div className='container-itemCart__title'>
+                <h1>{e.item.title}</h1>
+                <h3>{e.item.description}</h3>
+              </div>
+              <div className='container-itemCart__count'>
+                <h1>{e.count}</h1>
+              </div>
+              <div className='container-itemCart__price'>
+                <h1>${e.item.price}</h1>
+              </div>
+            </div>
+          ))
+        }
     </>
   )
 };
