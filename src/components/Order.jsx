4c22205886b2983f38adf93ee0be6499e0a276ee
Matias Martinez <martinez_matias@live.con.ar>
2022-02-26 01:17:47 -0300

    order

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
-import React, { useRef, useContext } from "react";
+import React, { useState, useRef, useContext } from "react";
 import { getFireStore } from "../db/firebase";
 import * as firebase from "firebase/app";
 import "firebase/firestore";
 import { cartContext } from "../context/CartProvider";
-import { useState } from "react/cjs/react.development";
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 
 function Order() {
   const { cart, deleteItem, clearCart, totalOrder } = useContext(cartContext);
   const { orderID, setOrderID } = useState();
+  const { shopFinish, setShopFinish } = useState(false);
 
   const nameRef = useRef();
   const lastNameRef = useRef();
@@ -35,120 +36,138 @@ function Order() {
         mail: mailRef.current.value,
         dni: documentRef.current.value,
         //sendType: sendTypeRef.current.value
+        totalOrder: totalOrder(),
+        items: cart,
       },
-      items: cart,
       //date: firebase.firestore.Timestamp.fromDate(new date()),
-      total: totalOrder(),
     };
     orders
       .add(newOrder)
-      .then(({ id }) => {
-        setOrderID(id);
-      })
+      .then(res => Swal.fire(
+        'Se realizo el pedido!',
+        'Numero de orden: ' + (res.id),
+        'Pedido Exitoso'
+      ))
       .catch((err) => {
-        alert("Ocurrio un error! " + err);
-      });
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: `${err}`
+      })
+    });
   }
 
   return (
-    <div className="container">
-      <div className="card order-card">
-        <form className="form-order">
-          <div className="form-row row">
-            <div className="form-group col-md-4">
-              <label className="form-label">Nombre</label>
-              <input
-                ref={nameRef}
-                type="name"
-                className="form-control"
-                id="inputEmail4"
-                placeholder="Nombre"
-              />
-            </div>
-            <div className="form-group col-md-4">
-              <label className="form-label">Apellido</label>
-              <input
-                ref={lastNameRef}
-                type="name"
-                className="form-control"
-                id="inputEmail4"
-                placeholder="Apellido"
-              />
-            </div>
-            <div className="form-group col-md-4">
-              <label className="form-label">Telefono de Contacto</label>
-              <input
-                ref={contactRef}
-                type="number"
-                className="form-control"
-                id="inputPassword4"
-                placeholder="Contacto"
-              />
-            </div>
+    <>
+      { !shopFinish ?
+        <div className="container">
+          <div className="card order-card">
+            <form className="form-order">
+              <div className="form-row row">
+                <div className="form-group col-md-4">
+                  <label className="form-label">Nombre</label>
+                  <input
+                    ref={nameRef}
+                    type="name"
+                    className="form-control"
+                    id="inputEmail4"
+                    placeholder="Nombre"
+                  />
+                </div>
+                <div className="form-group col-md-4">
+                  <label className="form-label">Apellido</label>
+                  <input
+                    ref={lastNameRef}
+                    type="name"
+                    className="form-control"
+                    id="inputEmail4"
+                    placeholder="Apellido"
+                  />
+                </div>
+                <div className="form-group col-md-4">
+                  <label className="form-label">Telefono de Contacto</label>
+                  <input
+                    ref={contactRef}
+                    type="number"
+                    className="form-control"
+                    id="inputPassword4"
+                    placeholder="Contacto"
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-md-4">
+                  <label className="form-label">Direccion</label>
+                  <input
+                    ref={adressRef}
+                    type="txt"
+                    className="form-control"
+                    placeholder="Direccion"
+                  />
+                </div>
+                <div className="form-group col-md-4">
+                  <label className="form-label">Localidad</label>
+                  <input
+                    ref={cityRef}
+                    type="txt"
+                    className="form-control"
+                    placeholder="Localidad"
+                  />
+                </div>
+                <div className="form-group col-md-4">
+                  <label className="form-label">Provincia</label>
+                  <input
+                    ref={provinceRef}
+                    type="txt"
+                    className="form-control"
+                    placeholder="Provincia"
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-md-6">
+                  <label className="form-label">Mail</label>
+                  <input
+                    ref={mailRef}
+                    type="txt"
+                    className="form-control"
+                    placeholder="Mail"
+                  />
+                </div>
+                <div className="form-group col-md-4">
+                  <label className="form-label">
+                    DNI
+                  </label>
+                  <input ref={documentRef} type="number" className="form-control" placeholder="DNI" />
+                </div>
+                {/* <div class="col-md-2">
+                  <label className="form-label">Envio a...</label>
+                  <select ref={sendType} class="form-select">
+                    <option selected>A Sucursal</option>
+                    <option>A domicilio</option>
+                  </select>
+                </div> */}
+              </div>
+              <div>
+                <Link to={"/order"}>
+                  <button onClick={() => finishOrder()} type="submit" className="btn btn-primary">
+                    Realizar Pedido
+                  </button>
+                </Link>
+              </div>
+            </form>
           </div>
-          <div className="row">
-            <div className="form-group col-md-4">
-              <label className="form-label">Direccion</label>
-              <input
-                ref={adressRef}
-                type="txt"
-                className="form-control"
-                placeholder="Direccion"
-              />
-            </div>
-            <div className="form-group col-md-4">
-              <label className="form-label">Localidad</label>
-              <input
-                ref={cityRef}
-                type="txt"
-                className="form-control"
-                placeholder="Localidad"
-              />
-            </div>
-            <div className="form-group col-md-4">
-              <label className="form-label">Provincia</label>
-              <input
-                ref={provinceRef}
-                type="txt"
-                className="form-control"
-                placeholder="Provincia"
-              />
-            </div>
+        </div>
+        :
+        <div>
+          <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading">Well done!</h4>
+            <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+            <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
           </div>
-          <div className="row">
-            <div className="form-group col-md-6">
-              <label className="form-label">Mail</label>
-              <input
-                ref={mailRef}
-                type="txt"
-                className="form-control"
-                placeholder="Mail"
-              />
-            </div>
-            <div className="form-group col-md-4">
-              <label ref={documentRef} className="form-label">
-                DNI
-              </label>
-              <input type="number" className="form-control" placeholder="DNI" />
-            </div>
-            {/* <div class="col-md-2">
-              <label className="form-label">Envio a...</label>
-              <select ref={sendType} class="form-select">
-                <option selected>A Sucursal</option>
-                <option>A domicilio</option>
-              </select>
-            </div> */}
-          </div>
-          <div>
-            <Link to={"/order"}>
-              <button onClick={() => finishOrder()} type="submit" className="btn btn-primary">
-                Realizar Pedido
-              </button>
-            </Link>
-          </div>
-        </form>
-      </div>
-    </div>
+        </div>
+      }
+    </>
   );
 }
 
